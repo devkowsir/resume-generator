0acf652c685c8fcf96fde76316df6f22076ebe3e
Kawsar Ahmed <72383564+devkowsir@users.noreply.github.com>
2024-12-01 14:32:34 +0600

    chore(client): set up Vite, React, TypeScript, ESLint, Tailwind, ShadcnUI

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,41 @@
 import pluginJs from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import reactBasePlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import reactRefreshPlugin from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { ecmaVersion: 'latest' } },
   {
-    files: ['**/*.{js,mjs,cjs,ts}'],
-    languageOptions: { ecmaVersion: 'latest' },
+    files: ['apps/client/**/*.{ts,tsx}'],
+    settings: { react: { version: '18.3' } },
+    languageOptions: {
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: './apps/client',
+      },
+      globals: { ...globals.browser },
+    },
+    plugins: {
+      react: reactBasePlugin,
+      'react-hooks': reactHooksPlugin,
+      'react-refresh': reactRefreshPlugin,
+    },
+    rules: {
+      ...reactBasePlugin.configs.recommended.rules,
+      ...reactBasePlugin.configs['jsx-runtime'].rules,
+      ...reactHooksPlugin.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
   },
   pluginJs.configs.recommended,
   eslintPluginPrettierRecommended,
+  ...tseslint.configs.recommended,
 ];
