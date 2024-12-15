05befd5498f9856c3cbe87ab1fba740ebc9e2c10
Kawsar Ahmed <72383564+devkowsir@users.noreply.github.com>
2024-12-15 23:53:08 +0600

    chore: added better ts support when querying data with non-sql form with drizzle

--- apps/server/src/databases/postgres/index.ts
+++ apps/server/src/databases/postgres/index.ts
@@ -1,4 +1,5 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { POSTGRES_URL } from '../../config';
+import * as schema from './schema';
 
-export const pg = drizzle(POSTGRES_URL!);
+export const pg = drizzle(POSTGRES_URL!, { schema });
