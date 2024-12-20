e5fd40aca9a6e964cc127ac73203c6fd8f799d39
Kawsar Ahmed <72383564+devkowsir@users.noreply.github.com>
2024-12-20 23:38:58 +0600

    refactor(server): better typescript management

--- apps/server/src/types/express.d.ts
+++ apps/server/src/types/express.d.ts
@@ -1,4 +1,4 @@
-import { usersTable } from '../databases/postgres/schema';
+import { TUserData } from '../databases/postgres/schema';
 
 // to make the file a module and avoid the TypeScript error
 export {};
@@ -6,7 +6,7 @@ export {};
 declare global {
   namespace Express {
     export interface Request {
-      user?: typeof usersTable.$inferSelect;
+      user?: Pick<TUserData, 'email' | 'name' | 'photo'>;
     }
   }
 }
--- apps/server/src/utils/helpers.ts
+++ apps/server/src/utils/helpers.ts
@@ -1,58 +1,52 @@
 // @ts-ignore
-type JSONCompatible =
-  | Record<any, any>
-  | Array<any>
-  | string
-  | number
-  | boolean
-  | null;
+type JSONCompatible = Record<any, any> | Array<any> | string | number | boolean | null;
 
 /**
  * This helper only helps sending JSON data.
  */
 export const fetchHelper = {
-  get: async (url: string, init: RequestInit = {}) => {
+  get: async <T = any>(url: string, init: RequestInit = {}) => {
     const response = await fetch(url, { method: 'GET', ...init });
-    return { response, data: await response.json() };
+    return { response, data: (await response.json()) as T };
   },
   /**
    * This helper only helps sending JSON data.
    */
-  post: async (url: string, body: JSONCompatible, init: RequestInit = {}) => {
+  post: async <T = any>(url: string, body: JSONCompatible, init: RequestInit = {}) => {
     const response = await fetch(url, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json', ...(init.headers ?? {}) },
       body: JSON.stringify(body),
       ...init,
     });
-    return { response, data: await response.json() };
+    return { response, data: (await response.json()) as T };
   },
   /**
    * This helper only helps sending JSON data.
    */
-  put: async (url: string, body: JSONCompatible, init: RequestInit = {}) => {
+  put: async <T = any>(url: string, body: JSONCompatible, init: RequestInit = {}) => {
     const response = await fetch(url, {
       method: 'PUT',
       headers: { 'Content-Type': 'application/json', ...(init.headers ?? {}) },
       body: JSON.stringify(body),
       ...init,
     });
-    return { response, data: await response.json() };
+    return { response, data: (await response.json()) as T };
   },
   /**
    * This helper only helps sending JSON data.
    */
-  patch: async (url: string, body: JSONCompatible, init: RequestInit = {}) => {
+  patch: async <T = any>(url: string, body: JSONCompatible, init: RequestInit = {}) => {
     const response = await fetch(url, {
       method: 'PATCH',
       headers: { 'Content-Type': 'application/json', ...(init.headers ?? {}) },
       body: JSON.stringify(body),
       ...init,
     });
-    return { response, data: await response.json() };
+    return { response, data: (await response.json()) as T };
   },
-  delete: async (url: string, init: RequestInit = {}) => {
+  delete: async <T = any>(url: string, init: RequestInit = {}) => {
     const response = await fetch(url, { method: 'DELETE', ...init });
-    return { response, data: await response.json() };
+    return { response, data: (await response.json()) as T };
   },
 };
