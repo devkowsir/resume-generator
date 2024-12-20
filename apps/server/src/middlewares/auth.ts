80b6cf950ef6da5768abddb0398feb09a4e125fa
Kawsar Ahmed <72383564+devkowsir@users.noreply.github.com>
2024-12-20 23:23:15 +0600

    refactor(server): auth middleware does not make any db checks

--- apps/server/src/middlewares/auth.ts
+++ apps/server/src/middlewares/auth.ts
@@ -1,43 +1,18 @@
-import { and, eq } from 'drizzle-orm';
 import { RequestHandler } from 'express';
 import { JsonWebTokenError, TokenExpiredError, verify } from 'jsonwebtoken';
 import { SECRET_KEY } from '../config';
-import { pg } from '../databases';
-import { usersTable } from '../databases/postgres/schema';
 import { HttpException } from '../exceptions/http';
-import { TTokenData } from '../types';
+import { TAccessTokenData } from '../types';
 
-export const authMiddleware: RequestHandler = async (req, res, next) => {
+export const authMiddleware: RequestHandler = (req, res, next) => {
   try {
-    const token =
-      req.cookies['Authorization'] ||
-      (req.header('Authorization')
-        ? req.header('Authorization')?.split('Bearer ')[1]
-        : null);
-
-    if (!token) {
-      return next(new HttpException(401, 'Authentication token missing.'));
-    }
-
-    const tokenData = verify(token, SECRET_KEY!) as TTokenData;
-    const findUser = await pg
-      .select()
-      .from(usersTable)
-      .where(
-        and(
-          eq(usersTable.email, tokenData.email),
-          eq(usersTable.id, tokenData.id),
-        ),
-      );
-    if (!findUser.length) {
-      return next(new HttpException(401, 'Wrong authentication token.'));
-    }
-
-    req.user = findUser[0];
+    const accessToken = req.headers.authorization?.split('Bearer ')[1];
+    if (!accessToken) return next(new HttpException(401, 'Invalid token.'));
+    req.user = verify(accessToken, SECRET_KEY!) as TAccessTokenData;
     next();
   } catch (error) {
     if (error instanceof TokenExpiredError) {
-      return next(new HttpException(401, 'Authorization token expired.'));
+      return next(new HttpException(401, 'Token expired.'));
     } else if (error instanceof JsonWebTokenError) {
       return next(new HttpException(401, 'Wrong authentication token.'));
     }
